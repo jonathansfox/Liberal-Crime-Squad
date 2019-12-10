83d57ff6ba68ef6652407ec1bc966dd56c1ecc7f
King-Drake <akingdrake@yahoo.com>
2019-12-09 20:02:16 -0600

    4.12.42 Updated version number

--- src/includes.h
+++ src/includes.h
@@ -987,8 +987,8 @@ string skill_enum_to_string(int skill_type);
 string showXmlSkill(int skill_, int value_);
 string showXmlAttribute(int attribute_, int value_);
 
-const string PACKAGE_VERSION_STR = "4.12.41";
-const int version = 41241;
+const string PACKAGE_VERSION_STR = "4.12.42";
+const int version = 41242;
 const int lowestloadversion = 40100;
 const int lowestloadscoreversion = 31203;
 
