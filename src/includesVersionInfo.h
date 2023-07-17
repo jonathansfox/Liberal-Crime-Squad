9be2e5fa9fef3b8b4d3d8b94de1376e4a019c0ef
King-Drake <akingdrake@yahoo.com>
2023-07-17 05:36:19 -0500

    Corrected Version Number 4.12.67

--- src/includesVersionInfo.h
+++ src/includesVersionInfo.h
@@ -1,4 +1,4 @@
-const string PACKAGE_VERSION_STR = "4.12.64";
-const int version = 41264;
+const string PACKAGE_VERSION_STR = "4.12.67";
+const int version = 41267;
 const int lowestloadversion = 40100;
 const int lowestloadscoreversion = 31203;
